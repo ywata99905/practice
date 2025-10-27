af4921fd48c93b4b2c03c56db2942b65b7c491c2
ywata99905 <ywata.99905@gmail.com>
2025-10-27 11:59:37 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,16 @@
+public abstract class TangibleAsset {
+  private String name;
+  private int price;
+  private String color;
+
+public TrangibleAsset(STring name, int price, String color) {
+  this.name=name;
+  this.price=price;
+  this.color=color;
+}
+
+public String getName(){return this.name}
+public int getPrice(){return this.price}
+public String getColor(){return this.color}
+
+}
